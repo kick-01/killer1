ce81d1272d01a453947fd83ccc924ebabedfec0e
! Okay <52571273+kick-01@users.noreply.github.com>
2022-01-01 20:57:56 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
   ID = ""
   go = True
   while go:
-    token = input("input you're bot TOKEN:")
+    token = input("your bot TOKEN:")
     get = Bot(token,"getme",{})
     if get["ok"]:
       out = out+"\n"+"TOKEN = '{}'\nBOT_ID = TOKEN.split(':')[0]".format(token)
@@ -28,7 +28,7 @@ API_HASH = '9f4461079f30757ca0a4c23e14bd523f'
     else:
       print("TOKEN is invalid, Try again")
 
-  sudo = input("input you're ID:")
+  sudo = input("youre ID:")
   out = out+"\n"+"SUDO = {}".format(sudo)
 
   f = open("config.py","w+") 
